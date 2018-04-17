d1b7bb8a1fe3d1c38c90d776d0d23db80843fd4c
Eagleciam <ciam042@gmail.com>
2018-04-16 22:45:48 -0500

    Add files via upload
    Este archivo contiene el script para crear, llenar y generar el usuario que controla la base de datos en MySQL

---
+++ FinanciaDataBaseScript.sql
@@ -0,0 +1,36 @@
+ -- Base de datos: `javaweb_financia` -
+ 
+ CREATE USER 'financiaUser'@'localhost' IDENTIFIED BY 'financia123';
+ 
+ GRANT ALL PRIVILEGES ON * . * TO 'financiaUser'@'localhost';
+ FLUSH PRIVILEGES;
+ 
+ 
+ CREATE  DATABASE IF NOT EXISTS javaweb_financia;
+ 
+ USE javaweb_financia;
+ 
+ 
+ SELECT * FROM socios WHERE monto_maximo>4200000;
+ 
+ -- Estructura de tabla para la tabla `articulos` -
+ 
+ CREATE TABLE `socios` ( 
+ `id` int(10) NOT NULL, 
+ `nombre` varchar(30) COLLATE utf8_spanish2_ci NOT NULL, 
+ `tasa` double NOT NULL, 
+ `monto_maximo` int(20) NOT NULL 
+ ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+ 
+ 
+ -- Volcado de datos para la tabla `articulos` -
+ INSERT INTO `socios` (`id`, `nombre`, `tasa`, `monto_maximo`) VALUES
+ (1, 'Juan', 1.5, 5000000),
+ (2, 'Andrés', 2.0, 7500000), 
+ (3, 'María', 1.2, 3000000);
+ 
+
+ -- Índices para tablas volcadas 
+ -- Indices de la tabla `articulos`
+ ALTER TABLE `socios` 
+ ADD PRIMARY KEY (`id`);
